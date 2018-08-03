c4d35982e715b14884829dc3f6aad3a8b4950925
Yang Chen <young.y.chen@gmail.com>
2018-08-03 00:22:28 -0700

    change Micro Chain to MicroChain, add network name

--- client/main.js
+++ client/main.js
@@ -9,3 +9,18 @@ import './templates/views/vnodeProtocolBaseAddr.js'
 Router.route('/', function () {
   this.render('home');
 });
+
+Template.home.helpers({
+  'getNetworkName': function(){
+    var networkName = Meteor.absoluteUrl();
+    if(networkName.indexOf("54.202.180.164")>0 || networkName.indexOf("nodes101")>0){
+      return "TestNet";
+    }
+    else if(networkName.indexOf("52.88.19.78")>0 || networkName.indexOf("nodes")>0){
+      return "MainNet";
+    }
+    else{
+      return "Dev";
+    }
+  }
+});
