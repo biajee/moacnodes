5cf8f1ffd87b25ce5ed284ad632ff8667c71635e
Yang Chen <young.y.chen@gmail.com>
2018-11-22 13:09:49 -0800

    use gateway

--- server/lib/moac/chain3Init.js
+++ server/lib/moac/chain3Init.js
@@ -6,4 +6,4 @@ global.Buffer = global.Buffer || require("buffer").Buffer;
 if(typeof chain3 !== 'undefined')
   chain3 = new Chain3(chain3.currentProvider);
 else
-  chain3 = new Chain3(new Chain3.providers.HttpProvider("http://127.0.0.1:8545"));
+  chain3 = new Chain3(new Chain3.providers.HttpProvider("http://gateway.moac.io/testnet"));
